493b62827532fbddc08986b0934c69aeb6b82463
JoshStaff99 <jstaffordmm10@gmail.com>
2025-05-14 12:15:56 +0100

    Project framework
    Project framework.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CSharp_MVC_AssessmentJS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
