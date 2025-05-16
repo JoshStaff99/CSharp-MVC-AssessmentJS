74b1cdd0df0f5c82b9ca3f64ddcdda3e852b1d8f
JoshStaff99 <jstaffordmm10@gmail.com>
2025-05-16 15:27:40 +0100

    Foreach loop, updated models and views
    Added a foreach loop to the companies and employees index page for each entry. Updated the Models and added a create, edit and show views.

--- Models/Company.cs
+++ Models/Company.cs
@@ -1,21 +1,23 @@
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 
-
-public class Company
+namespace CSharp_MVC_AssessmentJS.Models
 {
-    public int Id { get; set; }
+    public class Company
+    {
+        public int Id { get; set; }
 
-    [Required]
-    public string Name { get; set; }
+        [Required]
+        public string Name { get; set; }
 
-    [EmailAddress]
-    public string? Email { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
 
-    public string? LogoPath { get; set; }
+        public string? LogoPath { get; set; }
 
-    [Url]
-    public string? Website { get; set; }
+        [Url]
+        public string? Website { get; set; }
 
-    public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
 }
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,20 +1,23 @@
 using System.ComponentModel.DataAnnotations;
 
-public class Employee 
+namespace CSharp_MVC_AssessmentJS.Models
 {
-    public int Id { get; set; }
+    public class Employee 
+    {
+        public int Id { get; set; }
 
-    [Required]
-    public string FirstName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
 
-    [Required]
-    public string LastName { get; set; }
+        [Required]
+        public string LastName { get; set; }
 
-    public int CompanyId { get; set; }
-    public Company Company { get; set; }
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
 
-    [EmailAddress]
-    public string? Email { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
 
-    public string? PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
 }
